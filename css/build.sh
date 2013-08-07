211a5474ce69ae39ccd5f24f00824796302c02fa
edoceo <code@edoceo.com>
2013-08-07 16:54:17 -0700

    Full on with Less CSS now

--- css/build.sh
+++ css/build.sh
@@ -1,22 +1,29 @@
 #!/bin/bash -x
 
 cat \
-  base.css \
-  grid.css \
+  base.less \
+  html.less \
+  grid.less \
   list.css \
   form.css \
   page.css \
   info.css \
   note.css \
   menu.css \
-  responsive.css > radix-full.css
+  debug.css \
+  > radix.less
 
 # perl -MCSS::Minifier -e 'print CSS::Minifier::minify(input => *STDIN)' < radix-full.css > radix.css
-sed -e :a -e '$!N; s/,\n/,/; ta' < radix-full.css > radix.css
+# sed -e :a -e '$!N; s/,\n/,/; ta' < radix-full.css > radix.css
 
-cat debug.css radix-full.css > radix-debug.css
+# cat debug.css radix-full.css > radix-debug.css
 
 # @see http://davidgolightly.blogspot.com/2007/07/one-line-css-minifier.html
 # $ cat sourcefile.css \
 #   | sed -e 's/^[ \t]*//g; s/[ \t]*$//g; s/\([:{;,]\) /\1/g; s/ {/{/g; s/\/\*.*\*\///g; /^$/d' \
 #   | sed -e :a -e '$!N; s/\n\(.\)/\1/; ta' >target.css
+
+lessc \
+	--compress \
+	--strict-units=on \
+	radix.less radix.css
