2e7e95e8f1cf5ebe47ae30c965449a45117636a2
edoceo <code@edoceo.com>
2014-01-01 14:59:29 -0800

    crop function

--- Radix/Image.php
+++ Radix/Image.php
@@ -167,4 +167,60 @@ class radix_image
 
         return $ret;
     }
+
+    /**
+		@param $src Source File
+		@param $dst Target File
+		@param $x1 Top, Left X
+		@param $y1 Left Side Y
+		@param $x2
+		@param $y2 Bottom
+	*/
+    public static function crop($src, $dst, $x1, $y1, $x2, $y2)
+    {
+        list($src_w, $src_h, $type, $attr) = getimagesize($src);
+        switch ($type) {
+        case IMAGETYPE_GIF:
+            $src_i = imagecreatefromgif($src);
+            break;
+        case IMAGETYPE_JPEG:
+            $src_i = imagecreatefromjpeg($src);
+            break;
+        case IMAGETYPE_PNG:
+            $src_i = imagecreatefrompng($src);
+            break;
+        default:
+            throw new Exception("Invalid Image Type: $type",__LINE__);
+        }
+        imagealphablending($src_i, true);
+
+        $dst_w = $x2 - $x1;
+        $dst_h = $y2 - $y1;
+
+        if (($dst_w <= 0) || ($dst_h <= 0)) {
+        	return false;
+        }
+
+        // Radix::dump("Target: $dst_w x $dst_h");
+        $dst_i = imagecreatetruecolor($dst_w,$dst_h);
+        // And Make a Transparent Background
+        imagealphablending($dst_i, false);
+        imagesavealpha($dst_i, true);
+
+        $tbg = imagecolorallocatealpha($dst_i, 255, 255, 255, 127);
+        imagefilledrectangle($dst_i, 0, 0, $dst_w, $dst_h, $tbg);
+        // Copy/Scale
+        imagecopyresampled($dst_i,$src_i, 0, 0, $x1, $y1, $dst_w, $dst_h, $dst_w, $dst_h);
+
+        imagedestroy($src_i);
+        $ret = imagepng($dst_i,$dst);
+        imagedestroy($dst_i);
+        if ($ret !== true) {
+            $x = error_get_last();
+            throw new Exception($x['message'],__LINE__);
+        }
+
+        return true;
+    }
+
 }
